e4fc61fe13742eb6832f3f3001c0055c97d6b6da
zhangshengjie <z.cejay@gmail.com>
2022-05-29 03:25:40 +0800

    avatar

--- frontend/src/config/prod.ts
+++ frontend/src/config/prod.ts
@@ -1,6 +1,14 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: z.cejay@gmail.com
+ * @Date: 2022-05-29 00:04:31
+ * @LastEditors: cejay
+ * @LastEditTime: 2022-05-29 03:24:58
+ */
 // 线上环境配置
 export default {
-    BridgeAddress: <string>'0x4E3ba0edcE8df1b20eE19FfF11fC32447933bf4c',
+    BridgeAddress: <string>'0xc413bffB3e567cFd871F6B7fB13F5EbbA9bDBc18',
     Chain: <ChainOption>{
         chainId: '0x' + (80001).toString(16),
         chainName: 'Mumbai',
