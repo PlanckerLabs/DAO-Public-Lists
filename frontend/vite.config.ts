428185fcf26fe3f5b668a09846789211cd7fabfc
pony <510351311@qq.com>
2022-05-29 03:01:05 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 const path = require("path");
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   resolve: {
     // 配置路径别名
