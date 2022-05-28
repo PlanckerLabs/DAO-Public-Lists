7f47828b2bf46be1a431fd88ed015392e4e6f2ee
zhangshengjie <z.cejay@gmail.com>
2022-05-29 01:55:05 +0800

    Update index.vue

--- frontend/src/views/my_dao/components/dao-square/index.vue
+++ frontend/src/views/my_dao/components/dao-square/index.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="top">
     <div class="search">
-      <el-input :input-style="searchInputStyle" v-model="searchContent"
-                placeholder="Please enter address search" maxlength="50"></el-input>
+      <el-input :input-style="searchInputStyle" v-model="searchContent" placeholder="Please enter address search"
+        maxlength="50"></el-input>
       <el-button color="#6E3FF5" class="search-btn" @click="onHandleSearch" :disabled="btnDisable" :loading="loading">
         Search
       </el-button>
     </div>
     <div style="margin-top: 0.04rem;"></div>
   </div>
-  <Dao :list="DaoList"/>
+  <Dao :list="DaoList" />
 </template>
 
 <script setup>
-import {onMounted, reactive, ref, watch} from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 import Dao from "./componets/Dao/index.vue";
 import User from "./componets/User/index.vue";
 import useContractTool from '@/utils/useContractTool';
-import {useStore} from "/src/store";
+import { useStore } from "/src/store";
 import _ from 'lodash';
 
 const store = useStore();
-const {Bridge_getStrings, Bridge_listDAO} = useContractTool();
+const { Bridge_getStrings, Bridge_listDAO, Bridge_userDetail } = useContractTool();
 const DaoList = reactive([]);
 // const {ContractCall, bridge} = useWeb3();
 const searchInputStyle = {
@@ -44,7 +44,30 @@ const DaoDetail = async (address) => {
 }
 
 onMounted(async () => {
-  let ret = await Bridge_listDAO();
+  let dataArr = [];
+  try {
+    dataArr = Promise.all(Bridge_listDAO(), Bridge_userDetail());
+  } catch (error) {
+    console.error(error);
+    ElMessage.error(error.message);
+  }
+  if (dataArr.length !== 2 || !dataArr[0] || !dataArr[1] || !(dataArr[0].medals) || !(dataArr[1].dao)) {
+    ElMessage.error('Failed to get the list of DAO');
+    return;
+  }
+  const [ret, userList] = dataArr;
+  const userMedalStatusMap = new Map();
+  for (const oneDAO of userList.dao) {
+    const _dao_address = oneDAO.address;
+    for (const oneMedal of oneDAO.medals) {
+      const _medalindex = oneMedal.medalindex;
+      const _status = oneMedal.status;
+      const _key = _dao_address + '_' + _medalindex;
+      userMedalStatusMap.set(_key, _status);
+    }
+  }
+
+
   // console.log(ret);
   for (let index in ret.medals) {
     let v = ret.medals[index];
@@ -55,41 +78,56 @@ onMounted(async () => {
 
     for (let a in v.medals) {
       let medal = v.medals[a];
-      medal.name = atob(medal.name);
-      medal.uri = atob(medal.uri);
+      medal.name = medal.name;
+      medal.uri = medal.uri;
       medal.applying = false;
-      medal.canApply = true;
+      medal.canApply = true; 
+      medal.rejected = false;
       medal.contract_address = ret.address[index];
-      // 判断该NFT申请状态  持有 申请中 被拒绝
-      await ContractCall(abi_bridge, bridge, 'getCliamRequest', [ret.address[index], 0, 9999]).then((res) => {
-        let requestList = JSON.parse(res);
-        requestList.forEach((v, index) => {
-          if (v.address === store.account && v.medalindex === medal.index) {
-            // 存在我的申请记录
-            switch (v.status) {
-              case 1:
-                // 申请中
-                medal.applying = true;
-                medal.canApply = false;
-                break;
-              case 2:
-                // 被拒绝
-                medal.canApply = false;
-                medal.canApply = false;
-                break;
-              default:
-                // 其他值  已申请成功  >2 tokenid
-                medal.applying = false;
-                medal.canApply = false;
-                break;
-            }
-          }
-        })
-        // console.log(medal);
-      })
+      const _key = medal.contract_address + '_' + a;
+      let _status = userMedalStatusMap.get(_key);
+      if (_status === undefined) {
+        _status = 0;
+      }
+      if (_status === 0) {  
+      } else if (_status === 1) {
+        medal.applying = true;
+        medal.canApply = false;
+      } else if (_status === 2) {  
+        medal.rejected = true;
+      }else {
+        medal.canApply = false;
+      }
+      // // 判断该NFT申请状态  持有 申请中 被拒绝
+      // await ContractCall(abi_bridge, bridge, 'getCliamRequest', [ret.address[index], 0, 9999]).then((res) => {
+      //   let requestList = JSON.parse(res);
+      //   requestList.forEach((v, index) => {
+      //     if (v.address === store.account && v.medalindex === medal.index) {
+      //       // 存在我的申请记录
+      //       switch (v.status) {
+      //         case 1:
+      //           // 申请中
+      //           medal.applying = true;
+      //           medal.canApply = false;
+      //           break;
+      //         case 2:
+      //           // 被拒绝
+      //           medal.canApply = false;
+      //           medal.canApply = false;
+      //           break;
+      //         default:
+      //           // 其他值  已申请成功  >2 tokenid
+      //           medal.applying = false;
+      //           medal.canApply = false;
+      //           break;
+      //       }
+      //     }
+      //   })
+      //   // console.log(medal);
+      // })
     }
     // console.log(v.medals);
-    DaoList.push(_.cloneDeep({detail: {name: atob(v.name)}, medals: v.medals}))
+    DaoList.push(_.cloneDeep({ detail: { name: atob(v.name) }, medals: v.medals }))
   }
   ;
   ret.address.forEach(async (address, index) => {
@@ -121,7 +159,6 @@ const onHandleSearch = () => {
 </script>
 
 <style lang="scss" scoped>
-
 .top {
   display: flex;
   height: 3.33rem;
@@ -137,5 +174,4 @@ const onHandleSearch = () => {
     }
   }
 }
-
 </style>
