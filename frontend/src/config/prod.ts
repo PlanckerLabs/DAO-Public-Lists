4c8b194f9a6fae72786cfdace8cf33f532336285
zhangshengjie <z.cejay@gmail.com>
2022-05-30 15:52:37 +0800

    update

--- frontend/src/config/prod.ts
+++ frontend/src/config/prod.ts
@@ -4,11 +4,11 @@
  * @Autor: z.cejay@gmail.com
  * @Date: 2022-05-29 00:04:31
  * @LastEditors: cejay
- * @LastEditTime: 2022-05-29 03:24:58
+ * @LastEditTime: 2022-05-30 15:35:23
  */
 // 线上环境配置
 export default {
-    BridgeAddress: <string>'0xc413bffB3e567cFd871F6B7fB13F5EbbA9bDBc18',
+    BridgeAddress: <string>'0x7257fDd92DD8C97CF556c58275d68EE3607c3Dfa',
     Chain: <ChainOption>{
         chainId: '0x' + (80001).toString(16),
         chainName: 'Mumbai',
--- frontend/src/views/index/index.vue
+++ frontend/src/views/index/index.vue
@@ -1,31 +1,60 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: z.cejay@gmail.com
+ * @Date: 2022-05-29 21:53:16
+ * @LastEditors: cejay
+ * @LastEditTime: 2022-05-30 15:27:03
+-->
 <template>
   <div class="bg">
-    <Header/>
+    <Header />
     <div class="flex justify-center" style="margin-top: 88px;">
       <div class="flex flex-column align-center">
         <div style="width: 100%">
           <div class="tip">
-            Welcome to the dao 2 dao
-            interstellar family
+            Thanks for using NFTtool
           </div>
         </div>
         <div class="mt-4 mb-4">
-          <video playsinline="" controls src="https://cdn.modao.cc/Default_video.mp4">
-            <source src="https://cdn.modao.cc/Default_video.mp4">
-            Video element is not supported in this browser.
-          </video>
+          <iframe id="ytplayer" width="560" height="315" src="https://www.youtube.com/embed/pajE2OuoYZI?autoplay=1&mute=1"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
         </div>
       </div>
     </div>
   </div>
   <!-- Third-party License -->
   <a target="_blank" style="color: #cdcdcd; float: right; font-size: 0.8rem; padding: 1rem;"
-     href="https://github.com/PlanckerLabs/DAO-Square/blob/main/Third-party%20License.md">Third&nbsp;party&nbsp;License</a>
+    href="https://github.com/PlanckerLabs/DAO-Square/blob/main/Third-party%20License.md">Third&nbsp;party&nbsp;License</a>
 
 </template>
 
 <script setup>
 import Header from '@/components/Header.vue';
+
+</script>
+<script type="text/javascript">
+function resizeVideo() {
+  // get window width
+  let width = window.innerWidth * 0.7;
+  let r = 560 / 315;
+  let height = width / r;
+  let iframe = document.getElementById('ytplayer');
+  iframe.style.height = Math.round(height) + 'px';
+  iframe.style.width = Math.round(width) + 'px';
+}
+// on documet ready
+document.addEventListener('DOMContentLoaded', () => {
+  // resize iframe
+  resizeVideo();
+});
+// on window resize
+window.addEventListener('resize', () => {
+  resizeVideo();
+});
+
 
 </script>
 
